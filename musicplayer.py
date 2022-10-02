3730c39107c14127278d003dba22f8734358bd93
jyotirmay22 <114859424+jyotirmay22@users.noreply.github.com>
2022-10-02 16:51:02 +0530

    Add files via upload

---
+++ musicplayer.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import pygame 
+from pygame import mixer 
+from tkinter import * 
+import os 
+def playsong(): 
+    currentsong=playlist.get(ACTIVE) 
+    print(currentsong) 
+    mixer.music.load(currentsong) 
+    songstatus.set("Playing") 
+    mixer.music.play() 
+def pausesong(): 
+    songstatus.set("Paused") 
+    mixer.music.pause() 
+def stopsong(): 
+    songstatus.set("Stopped") 
+    mixer.music.stop() 
+def resumesong(): 
+    songstatus.set("Resuming") 
+    mixer.music.unpause() 
+root=tk.Tk() 
+root.title('music player-python project') 
+mixer.init() 
+songstatus=StringVar() 
+songstatus.set("choosing")
+
+    
+#playlist————— 
+playlist=Listbox(root,selectmode=SINGLE,bg="pink",fg="black",font=('Comic Sans MS',15),width=40) 
+playlist.grid(columnspan=5) 
+os.chdir(r'C:/Users/91836/Desktop/Music_player/songs') 
+songs=os.listdir()
+
+for s in songs: 
+    playlist.insert(END,s) 
+    playbtn=Button(root,text="Play",command=playsong) 
+    playbtn.config(font=('Bradley Hand ITC',20),bg="DodgerBlue2",fg="black",padx=7,pady=7)
+    playbtn.grid(row=1,column=0) 
+    pausebtn=Button(root,text="Pause",command=pausesong) 
+    pausebtn.config(font=('Bradley Hand ITC',20),bg="DodgerBlue2",fg="black",padx=7,pady=7)
+    pausebtn.grid(row=1,column=1) 
+    stopbtn=Button(root,text="Stop",command=stopsong) 
+    stopbtn.config(font=('Bradley Hand ITC',20),bg="DodgerBlue2",fg="black",padx=7,pady=7)
+    stopbtn.grid(row=1,column=2) 
+    Resumebtn=Button(root,text="Resume",command=resumesong) 
+    Resumebtn.config(font=('Bradley Hand ITC',20),bg="DodgerBlue2",fg="black",padx=7,pady=7)
+    Resumebtn.grid(row=1,column=3) 
+mainloop() 
